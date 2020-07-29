ea87d776b337fe7fbfe379466287908f6bf0bedc
towardsbackwards <Protuberance23>
2020-07-29 13:11:02 +0300

    Initial commit

---
+++ launcher.py
@@ -0,0 +1,21 @@
+import subprocess
+
+from common.variables import CLIENTS_COUNT_DEF
+
+process = []
+
+while True:
+    action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
+    if action == 'q':
+        break
+    elif action == 's':
+        clients_count = int(input('Введите количество тестовых клиентов для запуска, '
+                                  'либо нажмите Enter для использования значения по умолчанию: ') or CLIENTS_COUNT_DEF)
+        # Запускаем сервер:
+        process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        # Запускаем клиентов:
+        for i in range(clients_count):
+            process.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+    elif action == 'x':
+        while process:
+            process.pop().kill()
---
+++ my_testing_file.py
@@ -0,0 +1,6 @@
+import ipaddress
+
+a = '127.0.0.1'
+b = '255.0.0.256'
+breakpoint()
+ip_ = ipaddress.ip_address(4294967296)
